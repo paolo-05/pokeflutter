85f4dd73a80c2b0edb07bf211981105057cbfbd9
Cristian Duciuc <cduciuc@sorint.com>
2022-12-18 18:43:53 +0100

    lesson13 q

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pokeflutter/utils/palette.dart';
 
+import 'widgets/pokemon_list.dart';
 import 'widgets/random_floating_button.dart';
 import 'widgets/bottom_nav_bar.dart';
 import 'widgets/styled_text.dart';
@@ -10,7 +11,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context);
+    final textTheme = Theme.of(context).textTheme;
 
     return Scaffold(
       floatingActionButton: const RandomFloatingButton(),
@@ -21,17 +22,18 @@ class HomePage extends StatelessWidget {
             padding: const EdgeInsets.only(top: 76, left: 24, right: 24),
             child: StyledText(
               text: "Pokédex",
-              style: textTheme.textTheme.displaySmall!,
+              style: textTheme.displaySmall!,
               textHeight: 44,
             )),
         Container(
           padding: const EdgeInsets.symmetric(horizontal: 24),
           child: Text(
             "Use the advanced search to find Pokémon by type, weakness, ability and more!",
-            style: textTheme.textTheme.bodyLarge
+            style: textTheme.bodyLarge
                 ?.copyWith(color: gray[400], height: 24 / 16),
           ),
-        )
+        ),
+        PokemonList(),
       ]),
     );
   }
